4e3453cd5da4d0e6be306edefbd6280c298099c3
DoebelnAtz <axel.ej.adlercreutz@gmail.com>
2025-01-24 19:49:26 +0200

    fix conflict

--- gradesa/src/app/api/hello/route.js
+++ gradesa/src/app/api/hello/route.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { db } from '../../../backend/db';
 
 export async function GET(request) {
   const result = await db.pool('SELECT now()');
   const now = result.rows[0].now;
   return Response.json({ message: 'Hello!', now });
-=======
-import { query } from '../../../backend/db';
-
-export async function GET(request) {
-  const result = await query('SELECT now()');
-  const now = result.rows[0].now;
-  return Response.json({ message: 'Hello, world!', now });
->>>>>>> master
 }
