d6399c8a7af9809edfa29bed367dda709a3c13e5
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-02-04 15:40:01 +0200

    Add controlled inputs to form

--- gradesa/src/app/auth/login/page.js
+++ gradesa/src/app/auth/login/page.js
@@ -1,13 +1,12 @@
 'use client'
+import { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const email = formData.get('email');
-    const password = formData.get('password');
-
     console.log('Email:', email);
     console.log('Password:', password);
   }
@@ -18,12 +17,28 @@ export default function Login() {
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label className="form-label" htmlFor="email">E-Mail-Adresse</label>
-          <input className="form-input" type="email" id="email" name="email" required />
+          <input
+            className="form-input"
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
 
         <div className="form-group">
           <label className="form-label" htmlFor="password">Passwort</label>
-          <input className="form-input" type="password" id="password" name="password" required />
+          <input
+            className="form-input"
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </div>
 
         <button type="submit" className="form-button">Einloggen</button>
