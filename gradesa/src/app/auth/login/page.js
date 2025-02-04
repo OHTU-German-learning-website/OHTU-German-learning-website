8e4d4b7907a3d938e2e5759bb930544c292c9ff0
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-02-04 10:12:43 +0200

    Add basic event handling for form

--- gradesa/src/app/auth/login/page.js
+++ gradesa/src/app/auth/login/page.js
@@ -1,8 +1,21 @@
+'use client'
+
 export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    console.log('Email:', email);
+    console.log('Password:', password);
+  }
+  
   return (
     <>
       <h1 className="auth-title">Anmeldung</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label className="form-label" htmlFor="email">E-Mail-Adresse</label>
           <input className="form-input" type="email" id="email" name="email" required />
@@ -16,5 +29,5 @@ export default function Login() {
         <button type="submit" className="form-button">Einloggen</button>
       </form>
     </>
-  )
+  );
 }
