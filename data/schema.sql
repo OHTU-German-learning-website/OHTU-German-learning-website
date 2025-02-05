a9d58f53833c1ea1872e2ad72dc76eb78b54a5a8
Visuri Emil A <evisuri@cubbli22-gold.local>
2025-02-05 13:32:47 +0200

    Add users table migration and update schema

--- data/schema.sql
+++ data/schema.sql
@@ -4,3 +4,14 @@ CREATE TABLE public.pgmigrate_migrations (
   execution_time_in_millis bigint NOT NULL,
   applied_at timestamp with time zone NOT NULL
 );
+
+CREATE SEQUENCE public.users_id_seq;
+
+CREATE TABLE public.users (
+  id integer PRIMARY KEY NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+  username character varying(255) UNIQUE NOT NULL,
+  email character varying(255) UNIQUE NOT NULL,
+  password_hash text NOT NULL
+);
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
