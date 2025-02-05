4aff986102443fc307b650d65fb2261208f1ace0
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2025-02-05 17:06:11 +0200

    Fixed style issues in code with Prettier

--- gradesa/src/app/layout.js
+++ gradesa/src/app/layout.js
@@ -27,5 +27,5 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
-  )
+  );
 }
--- gradesa/src/components/ui/sidenavigation.js
+++ gradesa/src/components/ui/sidenavigation.js
@@ -1,40 +1,39 @@
-'use client'
+"use client";
 
-import { useState } from 'react';
-import Link from 'next/link'
+import { useState } from "react";
+import Link from "next/link";
 import "./sidebar.css";
 
 const Sidebar = () => {
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
-    const toggleDropdown = () => {
-        setDropdownOpen(previousState => !previousState)
-    };
+  const toggleDropdown = () => {
+    setDropdownOpen((previousState) => !previousState);
+  };
 
-    return (
-        <nav className="sidebar">
-            {/* Layout UI */}
-            <div className='sidebar-left'>
-                <h2>Lernen</h2>
+  return (
+    <nav className="sidebar">
+      {/* Layout UI */}
+      <div className="sidebar-left">
+        <h2>Lernen</h2>
+      </div>
+
+      <div className="sidebar-left nav-links">
+        <div className="dropdown">
+          <button onClick={toggleDropdown} className="dropdown-button">
+            Lektionen
+          </button>
+          {/* Dropdown list items */}
+          {isDropdownOpen && (
+            <div className="dropdown-menu">
+              <Link href="#">Grammatik 1</Link> <br />
+              <Link href="#">Grammatik 2</Link>
             </div>
-
-            <div className="sidebar-left nav-links">
-                <div className="dropdown">
-                    <button onClick={toggleDropdown} className="dropdown-button">
-                        Lektionen
-                    </button>
-                    {/* Dropdown list items */}
-                    {isDropdownOpen && (
-                    <div className="dropdown-menu">    
-                        <Link href="#">Grammatik 1</Link> <br />
-                        <Link href="#">Grammatik 2</Link>
-                    </div>               
-                    )}
-
-                </div>
-            </div>
-        </nav>
-    );
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Sidebar;
