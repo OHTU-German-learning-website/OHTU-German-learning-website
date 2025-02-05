2beaf8eef3d6b36d616156303ea9c7b9135d9482
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2025-02-05 11:17:56 +0200

    Added basic navigation bar

--- gradesa/src/components/ui/navigation.js
+++ gradesa/src/components/ui/navigation.js
@@ -1,12 +1,26 @@
-export default function Navbar({ children }) {
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Navbar = () => {
     return (
-      <html lang="en">
-        <body>
-          {/* Layout UI */}
-          <h1>Here is the navigation!</h1>
-          {/* Place children where you want to render a page or nested layout */}
-          <main>{children}</main>
-        </body>
-      </html>
+        <nav className="navbar">
+            {/* Layout UI */}
+            <div>
+                <Image 
+                src="/logo_placeholder.png"
+                width={240}
+                height={190}
+                alt="Logo placeholder"
+                />
+                <h1>Here is the navigation!</h1>
+            </div> 
+            <ul className="nav-links">
+                <li><Link href="#">Home</Link></li>
+                <li><Link href="#">About</Link></li>
+                <li><Link href="#">Contact</Link></li>
+            </ul>
+        </nav>
     );
-}
+};
+
+export default Navbar;
