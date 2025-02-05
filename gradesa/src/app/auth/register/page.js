858b56fee0b8a7356e98881fbb46e8bda57eaf15
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-02-05 18:56:15 +0200

    fix register

--- gradesa/src/app/auth/register/page.js
+++ gradesa/src/app/auth/register/page.js
@@ -1,59 +1,56 @@
-'use client'
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
 export default function Register() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (event) => {
-        event.preventDefault();
-        if (email === "" || password === "") {
-            setError(true);
-        } else {
-            setSubmitted(true);
-            setError(false);
-        }
-    };
-    const successMessage = () => {
-        return (
-            <div
-                className="success"
-                style={{
-                    display: submitted ? "" : "none",
-                }}
-            >
-                <h1>Benutzer {email} erfolgreich registriert</h1>
-            </div>
-        );
-    };
-    const errorMessage = () => {
-        return (
-            <div
-                className="error"
-                style={{
-                    display: error ? "" : "none",
-                }}
-            >
-                <h1>Bitte alle Felder ausfüllen</h1>
-            </div>
-        );
-    };
+    event.preventDefault();
+    if (email === "" || password === "") {
+      setError(true);
+    } else {
+      setSubmitted(true);
+      setError(false);
+    }
+  };
+  const successMessage = () => {
+    return (
+      <div className="success">
+        {!!submitted && <h1>Benutzer {email} erfolgreich registriert</h1>}
+      </div>
+    );
+  };
 
-  
+  const errorMessage = () => {
+    return (
+      <div
+        className="error"
+        style={{
+          display: error ? "" : "none",
+        }}
+      >
+        <h1>Bitte alle Felder ausfüllen</h1>
+      </div>
+    );
+  };
+
   return (
     <>
       <h1 className="auth-title">Registrieren</h1>
       <div className="messages">
-                {errorMessage()}
-                {successMessage()}
-        </div>
+        {errorMessage()}
+        {successMessage()}
+      </div>
 
       <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <label className="form-label" htmlFor="email">E-Mail-Adresse</label>
+          <label className="form-label" htmlFor="email">
+            E-Mail-Adresse
+          </label>
           <input
             className="form-input"
             type="email"
@@ -66,7 +63,9 @@ export default function Register() {
         </div>
 
         <div className="form-group">
-          <label className="form-label" htmlFor="password">Passwort</label>
+          <label className="form-label" htmlFor="password">
+            Passwort
+          </label>
           <input
             className="form-input"
             type="password"
@@ -78,7 +77,9 @@ export default function Register() {
           />
         </div>
 
-        <button onClick={handleSubmit} type="submit" className="form-button">Registrieren</button>
+        <button onClick={handleSubmit} type="submit" className="form-button">
+          Registrieren
+        </button>
       </form>
     </>
   );
