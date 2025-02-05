ec43e4e82100fb9933d352bcdeb6ee5df30df26d
rautiais <rautiainen.e.salla@gmail.com>
2025-02-05 17:08:43 +0200

    Some fixes to lessons-page and cleaning the code

--- gradesa/src/app/lessons/page.js
+++ gradesa/src/app/lessons/page.js
@@ -4,16 +4,10 @@ export default function Page() {
   // return <h1>Lessons!</h1>
 
   return (
-    <>
-      <Image 
-          src="/logo_placeholder.png"
-          width={240}
-          height={190}
-          alt="Logo placeholder"
-          />
+    <div className='lesson'>
       <h1 className="lessons-title">Lessons</h1>
       <h2 className="Grammatik1">Grammatik1</h2>
       <h2 className="Grammatik1">Grammatik2</h2>
-    </>
+    </div>
   );
 }
