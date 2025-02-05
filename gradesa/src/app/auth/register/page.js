051b5873e58ea08d5ba81f2f8c9461a8de8c0798
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-02-05 16:20:00 +0200

    add register page

---
+++ gradesa/src/app/auth/register/page.js
@@ -0,0 +1,88 @@
+'use client'
+import { useState } from 'react';
+
+export default function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (email === "" || password === "") {
+            setError(true);
+        } else {
+            setSubmitted(true);
+            setError(false);
+            const response = await fetch('/api/hello?mode=normal');
+            const data = await response.json();
+            console.log(data);
+        }
+    };
+    const successMessage = () => {
+        return (
+            <div
+                className="success"
+                style={{
+                    display: submitted ? "" : "none",
+                }}
+            >
+                <h1>Benutzer {email} erfolgreich registriert</h1>
+            </div>
+        );
+    };
+    const errorMessage = () => {
+        return (
+            <div
+                className="error"
+                style={{
+                    display: error ? "" : "none",
+                }}
+            >
+                <h1>Bitte alle Felder ausfüllen</h1>
+            </div>
+        );
+    };
+
+  
+  return (
+    <>
+      <h1 className="auth-title">Registrieren</h1>
+      <div className="messages">
+                {errorMessage()}
+                {successMessage()}
+        </div>
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label className="form-label" htmlFor="email">E-Mail-Adresse</label>
+          <input
+            className="form-input"
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="form-label" htmlFor="password">Passwort</label>
+          <input
+            className="form-input"
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button onClick={handleSubmit} type="submit" className="form-button">Registrieren</button>
+      </form>
+    </>
+  );
+}
