0f021c038f240dfeffdc17a5e664b48b969ba03f
rautiais <rautiainen.e.salla@gmail.com>
2025-02-13 13:33:13 +0200

    Added two more grammatik boxes. Made show more -links to each box and some css-styling to links

--- gradesa/src/app/lessons/page.js
+++ gradesa/src/app/lessons/page.js
@@ -1,6 +1,33 @@
+"use client";
+import { useState } from "react";
 import LessonsLayout from "./layout";
 
 export default function LessonsPage() {
+  const [showMoreGrammatik1, setShowMoreGrammatik1] = useState(false);
+  const [showMoreGrammatik2, setShowMoreGrammatik2] = useState(false);
+  const [showMoreGrammatik3, setShowMoreGrammatik3] = useState(false);
+  const [showMoreGrammatik4, setShowMoreGrammatik4] = useState(false);
+
+  const handleShowMoreGrammatik1 = (e) => {
+    e.preventDefault();
+    setShowMoreGrammatik1(!showMoreGrammatik1);
+  };
+
+  const handleShowMoreGrammatik2 = (e) => {
+    e.preventDefault();
+    setShowMoreGrammatik2(!showMoreGrammatik2);
+  };
+
+  const handleShowMoreGrammatik3 = (e) => {
+    e.preventDefault();
+    setShowMoreGrammatik3(!showMoreGrammatik3);
+  };
+
+  const handleShowMoreGrammatik4 = (e) => {
+    e.preventDefault();
+    setShowMoreGrammatik4(!showMoreGrammatik4);
+  };
+
   return (
     <LessonsLayout>
       <div>
@@ -12,7 +39,18 @@ export default function LessonsPage() {
               <li>Übung 1</li>
               <li>Übung 2</li>
               <li>Übung 3</li>
+              {showMoreGrammatik1 && (
+                <>
+                  <li className="more">Übung 4</li>
+                  <li className="more">Übung 5</li>
+                </>
+              )}
             </ul>
+            <div className="show-list">
+              <a href="#" id="more" onClick={handleShowMoreGrammatik1}>
+                {showMoreGrammatik1 ? "weniger anzeigen" : "mehr anzeigen"}
+              </a>
+            </div>
           </div>
           <div className="flex-child-element">
             <h2>Grammatik 2</h2>
@@ -20,7 +58,58 @@ export default function LessonsPage() {
               <li>Übung 1</li>
               <li>Übung 2</li>
               <li>Übung 3</li>
+              {showMoreGrammatik2 && (
+                <>
+                  <li className="more">Übung 4</li>
+                  <li className="more">Übung 5</li>
+                </>
+              )}
             </ul>
+            <div className="show-list">
+              <a href="#" id="more" onClick={handleShowMoreGrammatik2}>
+                {showMoreGrammatik2 ? "weniger anzeigen" : "mehr anzeigen"}
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex-parent-element">
+          <div className="flex-child-element">
+            <h2>Grammatik 3</h2>
+            <ul>
+              <li>Übung 1</li>
+              <li>Übung 2</li>
+              <li>Übung 3</li>
+              {showMoreGrammatik3 && (
+                <>
+                  <li className="more">Übung 4</li>
+                  <li className="more">Übung 5</li>
+                </>
+              )}
+            </ul>
+            <div className="show-list">
+              <a href="#" id="more" onClick={handleShowMoreGrammatik3}>
+                {showMoreGrammatik3 ? "weniger anzeigen" : "mehr anzeigen"}
+              </a>
+            </div>
+          </div>
+          <div className="flex-child-element">
+            <h2>Grammatik 4</h2>
+            <ul>
+              <li>Übung 1</li>
+              <li>Übung 2</li>
+              <li>Übung 3</li>
+              {showMoreGrammatik4 && (
+                <>
+                  <li className="more">Übung 4</li>
+                  <li className="more">Übung 5</li>
+                </>
+              )}
+            </ul>
+            <div className="show-list">
+              <a href="#" id="more" onClick={handleShowMoreGrammatik4}>
+                {showMoreGrammatik4 ? "weniger anzeigen" : "mehr anzeigen"}
+              </a>
+            </div>
           </div>
         </div>
       </div>
