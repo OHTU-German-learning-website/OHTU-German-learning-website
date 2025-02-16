81727a5ec61a1155a9c3164b00365ee2c21e9ef5
rautiais <rautiainen.e.salla@gmail.com>
2025-02-16 14:58:03 +0200

    Changed the ui for exercise-page

--- gradesa/src/app/lessons/exercises/layout.js
+++ gradesa/src/app/lessons/exercises/layout.js
@@ -8,4 +8,4 @@ export default function LessonsLayout({ children }) {
       <section className="lessons-content">{children}</section>
     </div>
   );
-}
+}3
--- gradesa/src/app/lessons/exercises/page.js
+++ gradesa/src/app/lessons/exercises/page.js
@@ -24,35 +24,19 @@ export default function LessonsPage() {
     <LessonsLayout>
       <div className="lessons-container">
         <h1>Grammatik 1</h1>
-        {initialGrammatikTopics.map((topic, index) => (
-          <div className="flex-parent-element" key={`${topic.title}-${index}`}>
-            <div className="flex-child-element">
-              <h2>{topic.title}</h2>
-              <ul>
-                {topic.exercises.slice(0, 3).map((exercise, exIndex) => (
-                  <li key={`${index}-exercise-${exIndex}`}>
-                    <button className="exercise-link">{exercise}</button>
-                  </li>
-                ))}
-                {grammatik[index] &&
-                  topic.exercises.slice(3).map((exercise, exIndex) => (
-                    <li key={`${index}-more-${exIndex}`} className="more">
-                      <button className="exercise-link">{exercise}</button>
-                    </li>
-                  ))}
-              </ul>
-              <div className="show-list">
-                <button
-                  className="show-more-link"
-                  onClick={(e) => toggleShowMore(e, index)}
-                >
-                  {grammatik[index] ? "weniger anzeigen" : "mehr anzeigen"}{" "}
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
+        <div className="flex-parent-element">
+          <ul className="exercise-list">
+            {initialGrammatikTopics[0].exercises.map((exercise, exIndex) => (
+              <li key={`exercise-${exIndex}`}>
+                <Link href={`/lessons/exercises`}>
+                  <button className="exercise-link">{exercise}</button>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+      <br></br>
       <Link href="/lessons">
         <button className="back-button">Back to lessons</button>
       </Link>
