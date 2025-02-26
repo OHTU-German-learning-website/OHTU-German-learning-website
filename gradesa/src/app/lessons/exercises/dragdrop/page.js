4e1b168276f85d4be43ce5654b71c6e9e3741c02
rautiais <rautiainen.e.salla@gmail.com>
2025-02-26 17:27:22 +0200

    Started making UI for drag-and-drop exercise template

---
+++ gradesa/src/app/lessons/exercises/dragdrop/page.js
@@ -0,0 +1,31 @@
+"use client";
+import styles from "../../../page.module.css";
+import DragdropLayout from "./layout";
+import { Grid } from "@radix-ui/themes";
+import { Box } from "@/components/ui/box/box";
+
+export default function Dragdrop({}) {
+  return (
+    <DragdropLayout>
+      <div className={styles.page}>
+        <div className="exercise-container">
+          <h1>Übung 1 Substantiv</h1>
+          <Box variant="outline" size="lg">
+            Kurs Schule Auto
+          </Box>
+          <Grid columns="3" gap="3" rows="2" width="auto" justify="center">
+            <Box gridrow="2 / 2" variant="outline" size="xl" minwidth="500px">
+              Kurs
+            </Box>
+            <Box gridrow="2 / 2" variant="outline" size="xl">
+              Schule
+            </Box>
+            <Box gridrow="2 / 2" variant="outline" size="xl">
+              Auto
+            </Box>
+          </Grid>
+        </div>
+      </div>
+    </DragdropLayout>
+  );
+}
