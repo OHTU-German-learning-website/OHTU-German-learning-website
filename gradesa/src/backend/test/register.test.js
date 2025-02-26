723544174717e1c89015056a28cab5d5988f1826
w1ll1ey <vili.ilari.kaisto@gmail.com>
2025-02-26 14:40:58 +0200

    fix tests

--- gradesa/src/backend/test/register.test.js
+++ gradesa/src/backend/test/register.test.js
@@ -2,97 +2,107 @@ import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
 import { useTestDatabase } from "./testdb";
 import { POST } from "../../app/api/auth/register/route";
 import { DB } from "../db";
+import { TestFactory } from "./testfactory";
+import { useTestRequest } from "@/backend/test/mock-request";
 
 describe("POST /register", () => {
   useTestDatabase();
   it("should return 409 if account already exists", async () => {
-    await DB.pool(
-      "INSERT INTO users (email, password_hash, salt) VALUES ($1, $2, $3)",
-      ["existinguser@example.com", "password123", "salt"]
-    );
+    const user = await TestFactory.user();
+    const { mockPost } = useTestRequest();
+    const email = user.email;
+    const password = user.password_hash;
 
-    const request = {
-      json: async () => ({
-        email: "existinguser@example.com",
-        password: "password123",
-      }),
-    };
+    const request = mockPost("@/api/auth/register", {
+      email,
+      password,
+    });
 
     const response = await POST(request);
     const result = await response.json();
 
     expect(response.status).toBe(409);
-    expect(result.message).toBe("Account already exists.");
+    expect(result.error).toBe("Account already exists.");
   });
 
   it("should return 400 if email and password are empty", async () => {
-    const request = {
-      json: async () => ({
-        email: "",
-        password: "",
-      }),
-    };
+    const email = "";
+    const password = "";
+    const { mockPost } = useTestRequest();
+
+    const request = mockPost("@/api/auth/register", {
+      email,
+      password,
+    });
 
     const response = await POST(request);
     const result = await response.json();
 
     expect(response.status).toBe(400);
-    expect(result.message).toBe("Email and password are required.");
+    expect(result.error).toBe("Email and password are required.");
   });
 
   it("should return 422 if email is invalid", async () => {
-    const request = {
-      json: async () => ({
-        email: "invalidemail",
-        password: "password123",
-      }),
-    };
+    const email = "invalidemail";
+    const password = "goodpassword123";
+    const { mockPost } = useTestRequest();
+
+    const request = mockPost("@/api/auth/register", {
+      email,
+      password,
+    });
 
     const response = await POST(request);
     const result = await response.json();
 
     expect(response.status).toBe(422);
-    expect(result.message).toBe("Invalid email address.");
+    expect(result.error).toBe("Invalid email address.");
   });
 
   it("should return 422 if password is too short", async () => {
-    const request = {
-      json: async () => ({
-        email: "validemail@email.com",
-        password: "short",
-      }),
-    };
+    const email = "validemail@email.com";
+    const password = "short";
+    const { mockPost } = useTestRequest();
+
+    const request = mockPost("@/api/auth/register", {
+      email,
+      password,
+    });
 
     const response = await POST(request);
     const result = await response.json();
 
     expect(response.status).toBe(422);
-    expect(result.message).toBe("Password must be at least 8 characters long.");
+    expect(result.error).toBe("Password must be at least 8 characters long.");
   });
 
   it("should return 422 if password is too long", async () => {
-    const request = {
-      json: async () => ({
-        email: "anothervalidemail@email.com",
-        password:
-          "thispasswordiswaytoolongandshouldnotbeacceptedbecauseitexceedsthemaximumlengthallowed",
-      }),
-    };
+    const email = "anothervalidemail@email.com";
+    const password =
+      "thispasswordiswaytoolongandshouldnotbeacceptedbecauseitexceedsthemaximumlengthallowed";
+    const { mockPost } = useTestRequest();
+
+    const request = mockPost("@/api/auth/register", {
+      email,
+      password,
+    });
 
     const response = await POST(request);
     const result = await response.json();
 
     expect(response.status).toBe(422);
-    expect(result.message).toBe("Password must be at least 8 characters long.");
+    expect(result.error).toBe("Password must be at least 8 characters long.");
   });
 
   it("should return 200 if account is created", async () => {
-    const request = {
-      json: async () => ({
-        email: "validanduniqueemail@email.com",
-        password: "goodpassword123",
-      }),
-    };
+    const email = "validanduniqueemail@email.com";
+    const password = "goodpassword123";
+    const { mockPost } = useTestRequest();
+
+    const request = mockPost("@/api/auth/register", {
+      email,
+      password,
+    });
 
     const response = await POST(request);
     const result = await response.json();
