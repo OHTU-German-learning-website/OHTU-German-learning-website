ab258d37dca1621b3f7e6563e5bdefe00558e757
DoebelnAtz <axel.ej.adlercreutz@gmail.com>
2025-02-27 15:03:55 +0200

    generic placeholder user

--- gradesa/src/backend/middleware/withAuth.js
+++ gradesa/src/backend/middleware/withAuth.js
@@ -1,5 +1,5 @@
 export function withAuth(callback) {
-  const user = { id: 1, email: "axel@withrealm.com" };
+  const user = { id: 1, email: "test@gradesa.com" };
   return async (request, ...args) => {
     request.user = user;
     return await callback(request, ...args);
