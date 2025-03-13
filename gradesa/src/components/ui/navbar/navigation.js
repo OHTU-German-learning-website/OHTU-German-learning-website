b9f487f132324c7e3f471c6e4c25d6a8dfceee2e
pronom8 <mangros19@gmail.com>
2025-03-13 13:19:43 +0200

    edited according to feedback some more

--- gradesa/src/components/ui/navbar/navigation.js
+++ gradesa/src/components/ui/navbar/navigation.js
@@ -1,21 +1,35 @@
 import Link from "next/link";
+import Image from "next/image";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="nav-brand">
-        <Link href="/">
-          <h2>Deutch lerner</h2>
-        </Link>
+      <div className="nav-left">
+        <div className="brand-container">
+          <div className="logo">
+            <Link href="/">
+              <Image
+                src="/logo_placeholder2.png"
+                width={50}
+                height={50}
+                alt="Logo placeholder"
+                priority={true}
+              />
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="nav-links"></div>
-      <div className="nav-auth">
-        <button className="login-btn">
-          <Link href="/auth/login">Anmelden</Link>
-        </button>
-        <button className="signup-btn">
-          <Link href="/auth/register">Registrieren</Link>
-        </button>
+
+      <div className="nav-right">
+        <div className="nav-links"></div>
+        <div className="nav-auth">
+          <button className="login-btn">
+            <Link href="/auth/login">Anmelden</Link>
+          </button>
+          <button className="signup-btn">
+            <Link href="/auth/register">Registrieren</Link>
+          </button>
+        </div>
       </div>
     </nav>
   );
