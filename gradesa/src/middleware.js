dc8cfe177dd1323edf11b9aefbad5d6b29b46705
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-03-18 10:12:42 +0200

    Add middleware for logged-in users only protected pages

--- gradesa/src/middleware.js
+++ gradesa/src/middleware.js
@@ -1,11 +1,20 @@
 import { NextResponse } from "next/server";
 import { checkSession } from "@/backend/auth/session";
 
+// Paths only logged-in users can access
+const protectedPaths = ["/lessons/exercises/dragdrop"];
+
+// Restricted paths logged-in users cannot access
 const restrictedPaths = ["/auth/login", "/auth/register"];
 
 export async function middleware(request) {
   const { pathname } = request.nextUrl;
   const userId = await checkSession();
+
+  // Redirect logged-out users trying to access protected paths
+  if (!userId && protectedPaths.includes(pathname)) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 
   // Redirect logged-in users trying to access the restricted paths
   if (userId && restrictedPaths.includes(pathname)) {
@@ -16,7 +25,7 @@ export async function middleware(request) {
   return NextResponse.next();
 }
 
-// Specify the routes where the middleware should run
+// Combine all the routes where the middleware should run
 export const config = {
-  matcher: restrictedPaths,
+  matcher: [...restrictedPaths, ...protectedPaths],
 };
