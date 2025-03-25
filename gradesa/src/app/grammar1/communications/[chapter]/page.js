33a8e31fb4ce3c9c9dd5648d4b55d9fdab7306d4
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2025-03-25 13:22:08 +0200

    added all 16 communications pages

--- gradesa/src/app/grammar1/communications/[chapter]/page.js
+++ gradesa/src/app/grammar1/communications/[chapter]/page.js
@@ -13,7 +13,7 @@ export default function Chapters() {
 
   const Chapter = chapters.find((c) => c.id === chapter);
   if (!Chapter) {
-    router.replace("/resources");
+    router.replace("grammar1/communications");
   }
 
   const previousChapter = chapters.find(
@@ -58,48 +58,113 @@ export default function Chapters() {
 export const chapters = [
   {
     id: "1",
-    title: "1. Über das Lernen",
+    title: "1. Über die Vergangenheit sprechen",
     content: Chapter1,
-    link: "/resources/1",
+    link: "/grammar1/communications/1",
   },
   {
     id: "2",
-    title: "2. Die Arten des Wissens",
+    title: "2. Anleitungen formulieren",
     content: Chapter2,
-    link: "/resources/2",
+    link: "/grammar1/communications/2",
   },
   {
     id: "3",
-    title:
-      "3. Was sind die Unterschiede zwischen Lernen im Klassenraum und allein lernen?",
+    title: "3. Offizielle Mitteilungen (Nachrichten usw.) formulieren",
     content: Chapter3,
-    link: "/resources/3",
+    link: "/grammar1/communications/3",
   },
   {
     id: "4",
-    title: "4. Was für ein Lerntyp bin ich?",
+    title:
+      "4. Höflich mit anderen Menschen umgehen (Sich vorstellen, Gäste empfangen und betreuen, Small Talk)",
     content: Chapter4,
-    link: "/resources/4",
+    link: "/grammar1/communications/4",
   },
   {
     id: "5",
-    title: "5.Freiwilliger Selbst-Test zu Lernstrategien",
+    title: "5. Menschen beschreiben / Ich und meine Familie",
     content: Chapter5,
-    link: "/resources/5",
+    link: "/grammar1/communications/5",
+  },
+  {
+    id: "6",
+    title: "6. Über Hobbys und Freizeit sprechen",
+    content: Chapter6,
+    link: "/grammar1/communications/6",
+  },
+  {
+    id: "7",
+    title: "7. Über Beruf und Arbeitsplatz sprechen",
+    content: Chapter7,
+    link: "/grammar1/communications/7",
+  },
+  {
+    id: "8",
+    title: "8. Firmen und Produkte beschreiben",
+    content: Chapter8,
+    link: "/grammar1/communications/8",
+  },
+  {
+    id: "9",
+    title: "9. Werbung machen",
+    content: Chapter9,
+    link: "/grammar1/communications/9",
+  },
+  {
+    id: "10",
+    title: "10. Einkaufen privat und geschäftlich",
+    content: Chapter10,
+    link: "/grammar1/communications/10",
+  },
+  {
+    id: "11",
+    title: "11. Räume beschreiben",
+    content: Chapter11,
+    link: "/grammar1/communications/11",
+  },
+  {
+    id: "12",
+    title: "12. Über historische Ereignisse sprechen",
+    content: Chapter12,
+    link: "/grammar1/communications/12",
+  },
+  {
+    id: "13",
+    title: "13. Pläne machen",
+    content: Chapter13,
+    link: "/grammar1/communications/13",
+  },
+  {
+    id: "14",
+    title:
+      "14. an Versammlungen und Geschäftstreffen teilnehmen und sie leiten",
+    content: Chapter14,
+    link: "/grammar1/communications/14",
+  },
+  {
+    id: "15",
+    title: "15. Jemandem gratulieren",
+    content: Chapter15,
+    link: "/grammar1/communications/15",
+  },
+  {
+    id: "16",
+    title: "16. Sich beschweren / etw. reklamieren",
+    content: Chapter16,
+    link: "/grammar1/communications/16",
   },
 ];
 
 function Chapter1() {
   return (
     <Column className="chapter-content">
-      <p>
-        Hier findest du einen Fragebogen, der von Fremdsprach-Didaktikern
-        entwickelt wurde. Den kannst du auf Deutsch oder Englisch ausfüllen. Er
-        soll dir zeigen, welche Lernstrategien du oP und gern verwendest. Dazu
-        rechnet der Computer die DurchschniOswerte der einzelnen Teile aus. Je
-        höher der Wert ist, desto mehr bevorzugst du Lernstrategien dieser
-        Bereiche. Dann kannst du für dich geeignete Übungen auswählen.
-      </p>
+      <ul>
+        <li>Perfekt</li>
+        <li>Präteritum</li>
+        <li>Plusquamperfekt</li>
+        <li>Saltzklammer</li>
+      </ul>
     </Column>
   );
 }
@@ -107,14 +172,11 @@ function Chapter1() {
 function Chapter2() {
   return (
     <Column className="chapter-content">
-      <p>
-        Hier findest du einen Fragebogen, der von Fremdsprach-Didaktikern
-        entwickelt wurde. Den kannst du auf Deutsch oder Englisch ausfüllen. Er
-        soll dir zeigen, welche Lernstrategien du oP und gern verwendest. Dazu
-        rechnet der Computer die DurchschniOswerte der einzelnen Teile aus. Je
-        höher der Wert ist, desto mehr bevorzugst du Lernstrategien dieser
-        Bereiche. Dann kannst du für dich geeignete Übungen auswählen.
-      </p>
+      <ul>
+        <li>Passiv</li>
+        <li>Infinitivkonstruktionen</li>
+        <li>Finalsätze u. a.</li>
+      </ul>
     </Column>
   );
 }
@@ -122,14 +184,12 @@ function Chapter2() {
 function Chapter3() {
   return (
     <Column className="chapter-content">
-      <p>
-        Hier findest du einen Fragebogen, der von Fremdsprach-Didaktikern
-        entwickelt wurde. Den kannst du auf Deutsch oder Englisch ausfüllen. Er
-        soll dir zeigen, welche Lernstrategien du oP und gern verwendest. Dazu
-        rechnet der Computer die DurchschniOswerte der einzelnen Teile aus. Je
-        höher der Wert ist, desto mehr bevorzugst du Lernstrategien dieser
-        Bereiche. Dann kannst du für dich geeignete Übungen auswählen.
-      </p>
+      <ul>
+        <li>Konjunktiv I bei indirekten Zitaten</li>
+        <li>indirekte Frage- und Aussagesätze</li>
+        <li>subjektive Modalität</li>
+        <li>Futur II u. a. Vermutungen</li>
+      </ul>
     </Column>
   );
 }
@@ -137,28 +197,159 @@ function Chapter3() {
 function Chapter4() {
   return (
     <Column className="chapter-content">
-      <p>
-        Hier findest du einen Fragebogen, der von Fremdsprach-Didaktikern
-        entwickelt wurde. Den kannst du auf Deutsch oder Englisch ausfüllen. Er
-        soll dir zeigen, welche Lernstrategien du oP und gern verwendest. Dazu
-        rechnet der Computer die DurchschniOswerte der einzelnen Teile aus. Je
-        höher der Wert ist, desto mehr bevorzugst du Lernstrategien dieser
-        Bereiche. Dann kannst du für dich geeignete Übungen auswählen.
-      </p>
+      <ul>
+        <li>Konjunktiv II als Höflichkeitsform</li>
+        <li>Das Pronomen es (z. B. bei Witterungsverben)</li>
+        <li>Dativ-Verben (geben, schenken usw.)</li>
+      </ul>
     </Column>
   );
 }
+
 function Chapter5() {
   return (
     <Column className="chapter-content">
-      <p>
-        Hier findest du einen Fragebogen, der von Fremdsprach-Didaktikern
-        entwickelt wurde. Den kannst du auf Deutsch oder Englisch ausfüllen. Er
-        soll dir zeigen, welche Lernstrategien du oP und gern verwendest. Dazu
-        rechnet der Computer die DurchschniOswerte der einzelnen Teile aus. Je
-        höher der Wert ist, desto mehr bevorzugst du Lernstrategien dieser
-        Bereiche. Dann kannst du für dich geeignete Übungen auswählen.
-      </p>
+      <ul>
+        <li>Vergleiche (wie und als)</li>
+        <li>Adjektivdeklination</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter6() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>Adjektivdeklination</li>
+        <li>Temporalangaben, -adverbien, -sätze</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter7() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>Hilfsverben</li>
+        <li>Artikelverwendung</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter8() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>Adjektivdeklination</li>
+        <li>Lokalangaben, -adverbien, -sätze</li>
+        <li>Passiv</li>
+        <li>
+          Adverbien zur Darstellung von Chronologie in Prozessbeschreibungen
+        </li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter9() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>Adjektivdeklination</li>
+        <li>Vergleiche</li>
+        <li>Präpositionen bei der Beschreibung von Leistungen und Produkten</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter10() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>Genuszuordnung</li>
+        <li>Formen der Artikel</li>
+        <li>Gebrauch der Artikel</li>
+        <li>andere Artikelwörter</li>
+        <li>Akkusativ Bildung und Verwendung</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter11() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>
+          Lokalpräpositionen (Wechsel- und ausgewählte lokale
+          Akk/Dat-Präpositionen)
+        </li>
+        <li>Lokaladverbien (insb. Verwendung im Satz)</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter12() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        Futur I<li>Vergangenheitstempora, auch (hyster.) Präsens</li>
+        <li>Temporaladverbien</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter13() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>Präsens + Temporaladverbien</li>
+        <li>Temporalangaben und -sätze</li>
+        <li>Konditionalsätze (real/irreal mit Konjunktiv II und würde)</li>
+        <li>Futur I</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter14() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>Temporaladverbien</li>
+        <li>Versch. Konjunktionalsätze</li>
+        <li>Uneingeleitete Nebensätze</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter15() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>Komparation der Adjektive</li>
+        <li>Anrede (du/Sie)</li>
+        <li>Perfekt / Präteritum</li>
+      </ul>
+    </Column>
+  );
+}
+
+function Chapter16() {
+  return (
+    <Column className="chapter-content">
+      <ul>
+        <li>Konjunktiv</li>
+        <li>Imperativ</li>
+        <li>Rektion der Verben</li>
+      </ul>
     </Column>
   );
 }
--- gradesa/src/app/grammar1/communications/page.js
+++ gradesa/src/app/grammar1/communications/page.js
@@ -4,10 +4,11 @@ import { useRouter } from "next/navigation";
 import { chapters } from "./[chapter]/page";
 import Link from "next/link";
 import { Column } from "@/components/ui/layout/container";
+
 export default function Chapter() {
   return (
     <Column gap="md">
-      <h1>Lernen</h1>
+      <h1>Kommunikationssituationen</h1>
       {chapters.map((chapter) => (
         <Link key={chapter.id} href={chapter.link}>
           {chapter.title}
