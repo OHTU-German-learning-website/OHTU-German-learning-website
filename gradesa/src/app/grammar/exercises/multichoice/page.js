dbd1a15f63f721e526e7a404d9d9802bf966a861
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-03-27 15:23:59 +0200

    Create multiple choice exercise page

---
+++ gradesa/src/app/grammar/exercises/multichoice/page.js
@@ -0,0 +1,9 @@
+import styles from "../../../page.module.css";
+
+export default function Multichoice() {
+  return (
+    <div className={styles.page}>
+      <h1>Multiple-Choice-Übung</h1>
+    </div>
+  );
+}
