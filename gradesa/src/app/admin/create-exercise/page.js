8bf0c5e6f344daee4743aa9e974ca89e43aaf5ec
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2025-04-22 17:59:33 +0300

    added linkbutton, linked dragdrop form

--- gradesa/src/app/admin/create-exercise/page.js
+++ gradesa/src/app/admin/create-exercise/page.js
@@ -1,15 +1,16 @@
 "use client";
 
 import { Column } from "@/components/ui/layout/container";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LinkButton } from "@/components/ui/linkbutton";
+
 export default function CreateExercise() {
   return (
-    <Column>
+    <Column gap="md">
       <h2>Create exercises</h2>
-      <Link href="/admin/create-exercise/free-form">
-        <Button>Free form</Button>
-      </Link>
+      <LinkButton href="/admin/create-exercise/free-form">Free form</LinkButton>
+      <LinkButton href="/admin/create-exercise/dragdrop">
+        Drag and drop
+      </LinkButton>
     </Column>
   );
 }
