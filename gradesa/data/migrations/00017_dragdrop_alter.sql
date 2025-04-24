4c0ddfac3119ecf5245f461354eab8a46a5cc494
DoebelnAtz <axel.ej.adlercreutz@gmail.com>
2025-04-24 14:15:26 +0300

    fix migration

--- gradesa/data/migrations/00017_dragdrop_alter.sql
+++ gradesa/data/migrations/00017_dragdrop_alter.sql
@@ -1,8 +1,4 @@
-BEGIN;
-
 ALTER TABLE dnd_categories
 ADD CONSTRAINT unique_category_color UNIQUE (category, color);
 
 ALTER TABLE draggable_words ADD CONSTRAINT unique_word UNIQUE (word);
-
-COMMIT;
