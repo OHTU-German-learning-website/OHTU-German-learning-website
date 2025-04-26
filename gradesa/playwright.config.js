bef8a2c1daac9b797a6922efa465ea04c56884ee
DoebelnAtz <axel.ej.adlercreutz@gmail.com>
2025-04-26 23:34:39 +0300

    fix test

--- gradesa/playwright.config.js
+++ gradesa/playwright.config.js
@@ -5,7 +5,7 @@ const { defineConfig, devices } = require("@playwright/test");
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: "./tests/e2e",
+  testDir: "./playwright/e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
