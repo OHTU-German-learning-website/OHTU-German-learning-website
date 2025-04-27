dfe46efe68edd707e580421b69da3f757bf23998
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2025-04-27 14:13:05 +0300

    fix

--- gradesa/src/app/not-found.js
+++ gradesa/src/app/not-found.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Custom404() {
   return (
-    <div className="justify-center">
+    <div>
       <h2>
         <Image src="/favicon.ico" alt="der Elefant" width={64} height={64} />
         404 - Page Not Found
