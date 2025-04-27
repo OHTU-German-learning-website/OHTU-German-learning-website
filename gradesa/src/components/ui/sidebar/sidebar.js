41d6f074e366a375646602b096ae422c4df7c485
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2025-04-27 16:03:14 +0300

    change links to sidebar style

--- gradesa/src/components/ui/sidebar/sidebar.js
+++ gradesa/src/components/ui/sidebar/sidebar.js
@@ -59,67 +59,103 @@ function StudentSideBar() {
       <Link className={styles.sidebarLink} href="/talkback">
         Rückmeldekanal-Feedback channel
       </Link>
-      <Dropdown
-        options={[
-          { label: "Glossar", value: "#" },
-          { label: "DeepL", value: "https://www.deepl.com/de/translator" },
-          {
-            label: "Google Translator",
-            value: "https://translate.google.com/",
-          },
-          { label: "ChatGPT", value: "https://chatgpt.com/ " },
-          { label: "Google Gemini", value: "https://gemini.google.com" },
-          {
-            label: "Wortschatz Uni Leipzig",
-            value: "https://wortschatz.uni-leipzig.de/de",
-          },
-          {
-            label: "Digitales Wörterbuch der Deutschen Sprache",
-            value: "https://www.dwds.de/",
-          },
-          {
-            label: "Institut für Deutsche Sprache",
-            value: "https://www.ids-mannheim.de/",
-          },
-          {
-            label: "Gesellschaft für Deutsche Sprache",
-            value: "https://gfds.de/",
-          },
-          { label: "Grammis", value: "https://grammis.ids-mannheim.de/" },
-          {
-            label: "Progr@mm",
-            value: "https://grammis.ids-mannheim.de/progr@mm",
-          },
-          {
-            label: "Hamburger Fernhochschule",
-            value:
-              "https://www.hfh-fernstudium.de/blog/welcher-lerntyp-bist-du",
-          },
-          {
-            label: "IQLingua",
-            value:
-              "https://www.iq-lingua.de/typ-checks/welcher-lerntyp-bin-ich/#c42954",
-          },
-          {
-            label: "Geolino",
-            value:
-              "https://www.geo.de/geolino/mensch/5849-rtkl-lernen-welcher-lerntyp-bist-du",
-          },
-          {
-            label: "Kapiert.de",
-            value: "https://www.kapiert.de/lerntypentest/",
-          },
-        ]}
-        trigger="click"
-        onSelect={(option) => {
-          window.location.href = option.value;
-        }}
-      >
-        <Button variant="outline">Links</Button>
-      </Dropdown>
+      <SidebarGroup title="Links" sublinks={extraLinks} topLink="#" />
     </>
   );
 }
+
+const extraLinks = [
+  {
+    title: "Glossar",
+    linkLabel: "Glossar",
+    link: "/glossary",
+    id: "glossary",
+  },
+  {
+    title: "DeepL",
+    linkLabel: "DeepL",
+    link: "https://www.deepl.com/de/translator",
+    id: "deepl",
+  },
+  {
+    title: "Google Translator",
+    linkLabel: "Google Translator",
+    link: "https://translate.google.com/",
+    id: "google-translator",
+  },
+  {
+    title: "ChatGPT",
+    linkLabel: "ChatGPT",
+    link: "https://chatgpt.com/",
+    id: "chatgpt",
+  },
+  {
+    title: "Google Gemini",
+    linkLabel: "Google Gemini",
+    link: "https://gemini.google.com",
+    id: "google-gemini",
+  },
+  {
+    title: "Wortschatz Uni Leipzig",
+    linkLabel: "Wortschatz Uni Leipzig",
+    link: "https://wortschatz.uni-leipzig.de/de",
+    id: "wortschatz-uni-leipzig",
+  },
+  {
+    title: "Digitales Wörterbuch der Deutschen Sprache",
+    linkLabel: "Digitales Wörterbuch der Deutschen Sprache",
+    link: "https://www.dwds.de/",
+    id: "digitales-woerterbuch-der-deutschen-sprache",
+  },
+  {
+    title: "Institut für Deutsche Sprache",
+    linkLabel: "Institut für Deutsche Sprache",
+    link: "https://www.ids-mannheim.de/",
+    id: "institut-fuer-deutsche-sprache",
+  },
+  {
+    title: "Gesellschaft für Deutsche Sprache",
+    linkLabel: "Gesellschaft für Deutsche Sprache",
+    link: "https://gfds.de/",
+    id: "gesellschaft-fuer-deutsche-sprache",
+  },
+  {
+    title: "Grammis",
+    linkLabel: "Grammis",
+    link: "https://grammis.ids-mannheim.de/",
+    id: "grammis",
+  },
+  {
+    title: "Progr@mm",
+    linkLabel: "Progr@mm",
+    link: "https://grammis.ids-mannheim.de/progr@mm",
+    id: "progr@mm",
+  },
+  {
+    title: "Hamburger Fernhochschule",
+    linkLabel: "Hamburger Fernhochschule",
+    link: "https://www.hfh-fernstudium.de/blog/welcher-lerntyp-bist-du",
+    id: "hamburger-fernhochschule",
+  },
+  {
+    title: "IQLingua",
+    linkLabel: "IQLingua",
+    link: "https://www.iq-lingua.de/typ-checks/welcher-lerntyp-bin-ich/#c42954",
+    id: "iq-lingua",
+  },
+  {
+    title: "Geolino",
+    linkLabel: "Geolino",
+    link: "https://www.geo.de/geolino/mensch/5849-rtkl-lernen-welcher-lerntyp-bist-du",
+    id: "geolino",
+  },
+  {
+    title: "Kapiert.de",
+    linkLabel: "Kapiert.de",
+    link: "https://www.kapiert.de/lerntypentest/",
+    id: "kapiert-de",
+  },
+];
 
 const adminSidebarLinks = [
   {
