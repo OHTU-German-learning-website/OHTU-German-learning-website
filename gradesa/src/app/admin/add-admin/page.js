63962b3d1d6ffa64c9618e793e11c9971c384106
Filippa Högnäsbacka <filippa.hognasbacka@gmail.com>
2025-09-21 10:57:51 +0300

    Added a message if user is added as admin succesfully

--- gradesa/src/app/admin/add-admin/page.js
+++ gradesa/src/app/admin/add-admin/page.js
@@ -6,15 +6,18 @@ import { useState } from "react";
 export default function AddAdmin() {
   const [email, setEmail] = useState("");
   const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
   const request = useRequest();
 
   const handleSubmit = async function (e) {
     e.preventDefault();
     try {
-      await request("admin/add-admin", { email });
+      const res = await request("admin/add-admin", { email });
+      setSuccess(res.data.message || "user added as admin successfully");
       setError(null); // nollaa virhe jos onnistuu
     } catch (err) {
       setError(err.message || "Tuntematon virhe");
+      setSuccess(null);
     }
   };
 
@@ -29,6 +32,7 @@ export default function AddAdmin() {
       <Button type="submit">Submit</Button>
 
       {error && <p style={{ color: "red", marginTop: "8px" }}>{error}</p>}
+      {success && <p style={{ color: "green", marginTop: "8px" }}>{success}</p>}
     </form>
   );
 }
