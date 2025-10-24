b8fca568980932ed5721242544dd427fe0cb938c
Filippa Högnäsbacka <filippa.hognasbacka@gmail.com>
2025-10-24 15:39:11 +0300

    Revert "commented in the code idea to fetch html content from db"
    This reverts commit d59f75f50821870ce5c47d90e502c7aefe40d133.

--- gradesa/src/app/api/html-content/[id]/route.js
+++ gradesa/src/app/api/html-content/[id]/route.js
@@ -1,4 +1,4 @@
-import { getHTMLContent } from "@/backend/html-services"; // ,updateHTMLContent
+import { getHTMLContent } from "@/backend/html-services";
 
 /*
 This route fetches html content from db according to id
@@ -21,14 +21,3 @@ export async function GET(req) {
     headers: { "Content-Type": "application/json" },
   });
 }
-
-//For saving edited content:
-// export async function PUT(req) {
-
-//  const url = new URL(req.url);
-//  const pathSegments = url.pathname.split("/");
-//  const id = pathSegments[pathSegments.length - 1];
-//  const body = await req.json();
-//  const {content} = body;
-
-//  await updateHTMLContent(id, content);
